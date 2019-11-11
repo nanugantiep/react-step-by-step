cfac976db9520a00099d8b96786238f754751fdd
nanugantiep <nanuganti@ebsco.com>
2019-11-11 15:51:21 -0500

    Adding router in the App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import Home from "../src/components/home";
+import About from "../src/components/about";
+import ToDO from "../src/components/todo";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/about">
+        <About />
+      </Route>
+      <Route path="/todo">
+        <ToDO />
+      </Route>
     </div>
-  );
+  </Router>)
 }
 
 export default App;
---
+++ src/components/todo/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ToDo = () => {
+
+    return (
+        <div>
+            <h2>This is TODO page</h2>
+        </div>
+    )
+}
+
+export default ToDo;
